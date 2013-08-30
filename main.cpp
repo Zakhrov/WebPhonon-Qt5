6c4400750d0a3f9a47bf9bc50ae369e0919d0631
zakhrov <aaron.zakhrov@gmail.com>
2013-08-30 09:12:42 +0530

    code cleanup

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+
     QSplashScreen splash;
     QPixmap pix;
     pix.load(":/Splash/WebPhonon-Master-text2.png");
     splash.setPixmap(pix);
     splash.show();
+    MainWindow w;
     w.showMaximized();
-    //QThread::sleep(5);
     splash.finish(&w);
     return a.exec();
 }
